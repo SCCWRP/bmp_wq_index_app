9383018320f79cd4b20c8d2457ba5bb171d197e0
Robert Butler <robertb@sccwrp.org>
2025-04-18 13:10:14 -0700

    round to two decimal places to match rounding convention of the LABMP app

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,4 @@
 # utils.R
-round2 <- function(x, digits) {
-  posneg = sign(x)
-  z = abs(x)*10^digits
-  z = z + 0.5 + sqrt(.Machine$double.eps)
-  z = trunc(z)
-  z = z/10^digits
-  z*posneg
-}
 
 format_axes <- function(decimals) {
   function(x) format(x, nsmall = decimals, scientific = FALSE, digits = 1)
