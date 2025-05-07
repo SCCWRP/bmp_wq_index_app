ad57bedfe6b658d39040e8157211ee4b1df137bb
Robert Butler <robertb@sccwrp.org>
2025-05-06 18:31:04 -0700

    hid wq tab

--- app.R
+++ app.R
@@ -9,7 +9,7 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Welcome", tabName = "Welcome", icon = icon("home")),
-      menuItem("Water Quality Performance", tabName = "WQ", icon = icon("check-circle")),
+      #menuItem("Water Quality Performance", tabName = "WQ", icon = icon("check-circle")),
       menuItem("Hydrology Performance", tabName = "Hydro", icon = icon("check-circle")),
       menuItem("Contact", tabName = "Contact", icon = icon("info-circle"))
     )
@@ -21,7 +21,7 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       welcome_tab,
-      wq_ui("wq"),
+      #wq_ui("wq"),
       hydro_ui("hydro"),
       contact_tab
     )
@@ -30,7 +30,7 @@ ui <- dashboardPage(
 
 # Server ----
 server <- function(input, output, session) {
-  wq_server("wq")
+  #wq_server("wq")
   hydro_server("hydro")
 }
 
